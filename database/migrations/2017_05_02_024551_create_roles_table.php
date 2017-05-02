e51796a524c6582ead6b69236fc56801b15223a3
Wilmer Mendoza <wil13ale@hotmail.com>
2017-05-01 22:50:45 -0400

    hecha las migraciones de la tabla roles y privileges

---
+++ database/migrations/2017_05_02_024551_create_roles_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateRolesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        //tabla de roles
+        Schema::create('roles',function(Blueprint $table){
+            $table->increments('id');
+            $table->string('nombre');
+            $table->string('tipo');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+        Schema::drop('roles');
+
+    }
+}
